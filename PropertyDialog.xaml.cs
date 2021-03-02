dacb4b5b0533f545616c948936d6ad74b54f5d7b
oalt <oa@lacheweg21.de>
2021-03-02 12:43:41 +0100

    Initial commit

---
+++ PropertyDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright (c) MDD4All.de, Dr. Oliver Alt
+ */
+using MDD4All.Configuration.ViewModels;
+using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Windows;
+
+namespace MDD4All.Configuration.Views
+{
+
+    public partial class PropertyDialog : Window
+    {
+        private ObservableCollection<PropertyDataViewModel> _propertyDataViewModels = new ObservableCollection<PropertyDataViewModel>();
+
+        public PropertyDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPropertyData(object data)
+        {
+            Type configurationObjectType = data.GetType();
+
+            _propertyDataViewModels.Clear();
+
+            foreach (PropertyInfo propertyInfo in configurationObjectType.GetProperties())
+            {
+                PropertyDataViewModel propertyDataViewModel = new PropertyDataViewModel(propertyInfo, data);
+
+                _propertyDataViewModels.Add(propertyDataViewModel);
+                
+            }
+
+            _propertyGrid.DataContext = _propertyDataViewModels;
+        }
+    }
+}
